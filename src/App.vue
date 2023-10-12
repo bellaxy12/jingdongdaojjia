bbac066ffb34c906860d9ecabad0f151b4d88613
bellaxy12 <273375387@qq.com>
2023-10-12 23:51:31 +0800

    docker search banner position布局实现

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,122 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="wrapper">
+    <div class="position">
+      <span class="position__icon iconfont">&#xe7f1;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="position__notice iconfont">&#xe70a;</span>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="search">
+      <div class="search__icon iconfont">&#xe67d;</div>
+      <span>山姆会员商店优惠商品</span>
+    </div>
+    <div class="banner">
+      <img 
+      src="http://www.dell-lee.com/imgs/vue3/banner.jpg" 
+      class="banner__img"
+      >
+    </div>
+  </div>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe674;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe826;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe713;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe78b;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style scoped lang="scss">
+@import '@/style/mixins.scss';
+@import '@/style/visible.scss';
+.wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: .5rem;
+  right: 0;
+  padding: 0 .18rem;
+  color: $content-fontcolor;
+  .position {
+    position: relative;
+    padding: .16rem 0;
+    padding-right: .24rem;
+    @include ellipse;
+    &__icon {
+      font-size: .16rem;
+      margin-right: .08rem;
+    }
+    &__notice {
+      position: absolute;
+      right: 0;
+      top: .12rem;
+      font-size: .2rem;
+    }
+  }
+  .search  {
+    vertical-align: middle;
+    line-height: .32rem;
+    background-color: #f5f5f5;
+    border-radius: .16rem;
+    font-size: .14rem;
+    color: #b7b7b7;
+    margin-bottom: 0.12rem;
+    &__icon {
+      display: inline-block;
+      font-size: .16rem;
+      padding-left: .16rem;
+      padding-right: .12rem;
+    }
+  }
+  .banner {
+    height: 0;
+    padding-bottom: 25.4%;
+    overflow: hidden;
+    &__img {
+      width: 100%;
+    }
+  }
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.docker {
+  display: flex;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  height: .49rem;
+  border-top: .01rem solid #f1f1f1;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  padding: 0 .18rem;
+  box-sizing: border-box;
+  color: $content-fontcolor;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      font-size: .18rem;
+      margin-top: .07rem;
+    }
+    &--active {
+      color: #1FA4FC;
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  &__title  {
+    font-size: .2rem;
+    transform: scale(.5, .5);
+    transform-origin: top center;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import 'normalize.css'
+import '@/style/index.scss'
 
 const app = createApp(App)
 
